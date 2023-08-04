37c3736ac97e7573266a26ea212df1a0dd663ae6
Fc-404 <33905772+Fc-404@users.noreply.github.com>
2023-08-04 22:42:19 +0800

    For Xiao Liu!

--- xazh/src/main.ts
+++ xazh/src/main.ts
@@ -1,3 +1,10 @@
+/**
+ * Today, I got a cat. 20230804-22:40.
+ * I'm going to call her, Xiao Liu.
+ * Haha, She is drinking milk.
+ */
+
+
 import { createApp } from 'vue'
 import App from './App.vue'
 
